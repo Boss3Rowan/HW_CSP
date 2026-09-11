0ade0df4b01daa84a514131921abb621f956a87d
Boss3Rowan <hazel.west@ucas-edu.net>
2026-09-11 14:42:09 -0600

    Variable pratice

---
+++ Variable_practice.py
@@ -0,0 +1,41 @@
+# HW, Variable practice
+
+name = "Hazel"
+
+number_1_to_10 = "2"
+
+number_100_to_1000 = "222"
+
+breakfast = "pancakes"
+
+favorite_color = "teal"
+
+school_name = "UCAS"
+
+year = "2026"
+
+eye_color = "hazel"
+
+age = "14"
+
+favorite_subject = "Biology"
+
+print(name)
+
+print(number_1_to_10)
+
+print(number_100_to_1000)
+
+print(breakfast)
+
+print(favorite_color)
+
+print(school_name)
+
+print(year)
+
+print(eye_color)
+
+print(age)
+
+print(favorite_subject)
