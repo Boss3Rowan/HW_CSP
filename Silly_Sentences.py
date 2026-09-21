bbac7a7fa75d9662e685a992e42a5baa7c535cab
Boss3Rowan <hazel.west@ucas-edu.net>
2026-09-21 14:52:38 -0600

    silly sentences

---
+++ Silly_Sentences.py
@@ -0,0 +1,17 @@
+# HW, 7th, Silly Sentences
+
+
+food = input("Tell me a food: ").strip().lower()
+noun1 = input("Tell me a noun: ").strip().lower()
+verb = input("Tell me a verb: ").strip().lower()
+noun2 = input("Tell me another noun: ").strip().lower()
+place1 = input("Tell me a place: ").strip().lower()
+adjective1 = input("Tell me an adjective: ").strip().lower()
+place2 = input("Tell me another place: ").strip().lower()
+adjective2 = input("Tell me another adjective: ").strip().lower()
+person = input("Tell me a person: ").strip().lower()
+noun3 = input("Tell me yet another noun: ").strip().lower()
+animal = input("Lastly, tell me an animal: ").strip().lower()
+
+
+print("At night I dream of "+ food +" that is as big as a "+ noun1 +". When I try to take a bite, it "+ verb +" on my "+ noun2 +". The best time I ever ate "+ food +" was at (the) "+ place1 +". It tasted "+ adjective1 +". But don't ever buy it at "+ place2 +", it is very "+ adjective2 +". My "+ person +" likes it best with "+ noun3 +". Be sure to give the leftovers to your "+ animal +".")
